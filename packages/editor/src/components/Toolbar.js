d7f7642087c2d2cd00fb141f58b4b96c284b9a9c
Brent Jackson <jxnblk@gmail.com>
2019-05-03 15:36:50 -0400

    Use React elements for Toolbar children

--- packages/editor/src/components/Toolbar.js
+++ packages/editor/src/components/Toolbar.js
@@ -1,4 +1,5 @@
 /** @jsx jsx */
+import React from 'react'
 import { jsx } from '@emotion/core'
 import { css } from 'theme-ui'
 
@@ -10,13 +11,9 @@ import ImageIcon from '@material-ui/icons/InsertPhoto'
 import QuoteIcon from '@material-ui/icons/FormatQuote'
 import CodeIcon from '@material-ui/icons/Code'
 import VideoIcon from '@material-ui/icons/Slideshow'
-import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted'
-import FormatListNumberedIcon from '@material-ui/icons/FormatListNumbered'
+import ListIcon from '@material-ui/icons/FormatListBulleted'
+import NumberedListIcon from '@material-ui/icons/FormatListNumbered'
 import HorizontalSplitIcon from '@material-ui/icons/HorizontalSplit'
-// import ListIcon from '@material-ui/icons/FormatListBulleted'
-// import StrikethroughIcon from '@material-ui/icons/StrikethroughS'
-// import HeadingIcon from '@material-ui/icons/Title'
-// import NumberedListIcon from '@material-ui/icons/FormatListNumbered'
 
 // "icons"
 const B = props => (
@@ -33,6 +30,8 @@ const H1 = () => <B>H1</B>
 const H2 = () => <B>H2</B>
 const JSX = () => <B>JSX</B>
 const Gist = () => <B>GIST</B>
+
+const Context = React.createContext(null)
 
 const Separator = () => (
   <div
@@ -63,144 +62,110 @@ const Root = props => (
   />
 )
 
-// config
-const buttons = [
-  {
-    title: 'Toggle Bold (⌘ B)',
-    Icon: BoldIcon,
-    command: 'toggleBold',
-    nodeType: 'bold'
-  },
-  {
-    title: 'Toggle Italic (⌘ I)',
-    Icon: ItalicIcon,
-    command: 'toggleItalic',
-    nodeType: 'italic'
-  },
-  {
-    title: 'Toggle Heading Level 1 (⌘ ⌥ 1)',
-    Icon: H1,
-    command: 'toggleHeadingOne',
-    nodeType: 'heading-one'
-  },
-  {
-    title: 'Toggle Heading Level 2 (⌘ ⌥ 2)',
-    Icon: H2,
-    command: 'toggleHeadingTwo',
-    nodeType: 'heading-two'
-  },
-  {
-    title: 'Toggle Block Quote (⌃ ⌥ Q)',
-    Icon: QuoteIcon,
-    command: 'toggleBlockQuote',
-    nodeType: 'block-quote'
-  },
-  {
-    title: 'Toggle Code Block',
-    Icon: CodeIcon,
-    command: 'togglePre',
-    nodeType: 'pre'
-  },
-  {
-    title: 'Insert Divider',
-    Icon: HorizontalSplitIcon,
-    command: 'toggleDivider',
-    nodeType: 'hr'
-  },
-  { separator: true },
-  {
-    title: 'Insert Link (⌘ K)',
-    Icon: LinkIcon,
-    command: 'toggleLink',
-    nodeType: 'link'
-  },
-  {
-    title: 'Insert Image',
-    Icon: ImageIcon,
-    command: 'insertImage',
-    nodeType: 'image'
-  },
-  { separator: true },
-  {
-    title: 'Insert Bulleted List',
-    Icon: FormatListBulletedIcon,
-    command: 'toggleBulletedList',
-    nodeType: 'bulleted-list'
-  },
-  {
-    title: 'Insert Numbered List',
-    Icon: FormatListNumberedIcon,
-    command: 'toggleNumberedList',
-    nodeType: 'numbered-list'
-  },
-  {
-    title: 'Insert JSX Block',
-    Icon: JSX,
-    command: 'toggleJSX',
-    nodeType: 'jsx'
-  },
-  { separator: true },
-  {
-    title: 'Insert YouTube Video',
-    Icon: VideoIcon,
-    command: 'insertYouTube',
-    nodeType: 'YouTube'
-  },
-  {
-    title: 'Insert GitHub Gist',
-    Icon: Gist,
-    command: 'insertGist',
-    nodeType: 'Gist'
-  }
-]
+// todo: handle custom jsx block types
+const Button = ({
+  nodeType,
+  // todo: is there a way to detect this?
+  mark,
+  ...props
+}) => {
+  const { editor } = React.useContext(Context)
+  const active = editor.isActive(nodeType)
+  return (
+    <button
+      {...props}
+      onClick={e => {
+        if (mark) {
+          editor.toggleMark(nodeType).focus()
+        } else {
+          editor.toggleBlock(nodeType).select()
+        }
+      }}
+      css={css({
+        display: 'block',
+        minWidth: 32,
+        height: 32,
+        padding: 1,
+        fontSize: 16,
+        lineHeight: 1.5,
+        margin: '1px',
+        color: active ? 'primary' : 'inherit',
+        backgroundColor: active ? 'lightgray' : 'transparent',
+        border: 0,
+        '&:focus': {
+          outline: '2px solid',
+          color: 'primary'
+        }
+      })}
+    />
+  )
+}
+
+const defaultChildren = (
+  <>
+    <Button mark title="Toggle Bold" nodeType="bold">
+      <BoldIcon />
+    </Button>
+    <Button mark title="Toggle Italic" nodeType="italic">
+      <ItalicIcon />
+    </Button>
+    <Button title="Heading 1 (⌘ ⌥ 1)" nodeType="heading-one">
+      <H1 />
+    </Button>
+    <Button title="Heading 2 (⌘ ⌥ 2)" nodeType="heading-two">
+      <H2 />
+    </Button>
+    <Button title="Block Quote (⌃ ⌥ Q)" nodeType="block-quote">
+      <QuoteIcon />
+    </Button>
+    <Button title="Code Block" nodeType="pre">
+      <CodeIcon />
+    </Button>
+    <Button title="Divider" nodeType="hr">
+      <HorizontalSplitIcon />
+    </Button>
+    <Separator />
+    <Button title="Link (⌘ K)" nodeType="link" mark>
+      <LinkIcon />
+    </Button>
+    <Button title="Image" nodeType="image">
+      <ImageIcon />
+    </Button>
+    <Separator />
+    <Button title="Bulleted List" nodeType="bulleted-list">
+      <ListIcon />
+    </Button>
+    <Button title="Numbered List" nodeType="numbered-list">
+      <NumberedListIcon />
+    </Button>
+    <Button title="JSX" nodeType="jsx">
+      <JSX />
+    </Button>
+    <Separator />
+    <Button title="YouTube Video" nodeType="YouTube">
+      <VideoIcon />
+    </Button>
+    <Button title="GitHub Gist" nodeType="Gist">
+      <Gist />
+    </Button>
+  </>
+)
 
 export const Toolbar = props => {
-  const { buttons, editor } = props
+  const { editor, children } = props
 
   return (
-    <Root>
-      {buttons.map(({ separator, Icon, title, command, nodeType }, i) =>
-        separator ? (
-          <Separator key={i} />
-        ) : (
-          <Toolbar.Button
-            key={i}
-            title={title}
-            active={editor.isActive(nodeType)}
-            onClick={editor[command]}
-          >
-            <Icon size={20} />
-          </Toolbar.Button>
-        )
-      )}
-    </Root>
+    <Context.Provider value={{ editor }}>
+      <Root>{children}</Root>
+    </Context.Provider>
   )
 }
 
 Toolbar.defaultProps = {
-  buttons
+  children: defaultChildren
 }
 
-Toolbar.Button = ({ active, ...props }) => (
-  <button
-    {...props}
-    css={css({
-      display: 'block',
-      minWidth: 32,
-      height: 32,
-      padding: 1,
-      fontSize: 16,
-      lineHeight: 1.5,
-      margin: '1px',
-      color: active ? 'primary' : 'inherit',
-      backgroundColor: active ? 'lightgray' : 'transparent',
-      border: 0,
-      '&:focus': {
-        outline: '2px solid',
-        color: 'primary'
-      }
-    })}
-  />
-)
+Toolbar.Button = Button
+Toolbar.Separator = Separator
 
 export default Toolbar
