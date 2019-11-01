55d08e27e3d900c504147666836feea41a74dbc4
John Otander <johnotander@gmail.com>
2019-11-01 09:29:56 -0600

    Cleanup

--- packages/builder/babel-plugin-remove-text-edit.js
+++ packages/builder/babel-plugin-remove-text-edit.js
@@ -2,14 +2,11 @@ export default () => {
   return {
     visitor: {
       JSXOpeningElement(path) {
-        // Don't add UUIDs to internal blocks elements. For now we'll
-        // prefix them with BLOCKS_
         const name = path.node.name && path.node.name.name
         if (name !== 'BLOCKS_Text') {
           return
         }
 
-        //console.log(path)
         path.parentPath.replaceWith(path.container.children[0])
       }
     }
