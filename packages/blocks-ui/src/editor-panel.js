0d7bd87255ea1dcf8117c8744a9354558febde21
John Otander <johnotander@gmail.com>
2019-11-22 19:54:07 -0700

    Move editor panel into its own file/component

---
+++ packages/blocks-ui/src/editor-panel.js
@@ -0,0 +1,178 @@
+/** @jsx jsx */
+import React from 'react'
+import { jsx } from 'theme-ui'
+import { Label, Input } from '@theme-ui/components'
+
+export default ({
+  elementData,
+  handleChange,
+  handlePropChange,
+  handleRemove,
+  handleRemoveElement,
+  handleParentSelect,
+  handleInsertElement,
+  handleClone,
+  handleTextUpdate,
+  setElementId,
+  blocks = [] // TODO: Fix
+}) => (
+  <div
+    sx={{
+      height: '100vh',
+      overflow: 'scroll'
+    }}
+  >
+    {elementData ? (
+      <h3
+        sx={{
+          textTransform: 'uppercase',
+          fontSize: 2,
+          fontWeight: 600,
+          m: 0,
+          px: 3,
+          py: 1,
+          borderBottom: 'thin solid #e1e6eb'
+        }}
+      >
+        {elementData.name}
+        <button onClick={handleRemoveElement}>Remove</button>
+        <button onClick={handleInsertElement}>Insert</button>
+        <button onClick={handleClone}>Clone</button>
+        {elementData.parentId && (
+          <button onClick={handleParentSelect}>Parent</button>
+        )}
+      </h3>
+    ) : (
+      <ul>
+        {blocks.map &&
+          blocks.map(block => (
+            <li key={block.id} onClick={() => setElementId(block.id)}>
+              {block.name}
+            </li>
+          ))}
+      </ul>
+    )}
+    <div>
+      {elementData && (
+        <div sx={{ px: 3 }}>
+          {elementData.hasOwnProperty('text') && (
+            <React.Fragment>
+              <Label id="text">Text</Label>
+              <Input
+                sx={{
+                  display: 'block',
+                  width: '100%'
+                }}
+                onChange={handleTextUpdate}
+                value={elementData.text}
+                aria-labelledby="text"
+              />
+            </React.Fragment>
+          )}
+          <h3
+            sx={{
+              fontWeight: 'normal',
+              m: 0,
+              pb: 2,
+              pt: 4
+            }}
+          >
+            Props
+          </h3>
+          {elementData.props.hasOwnProperty('to') && (
+            <React.Fragment>
+              <Label id="to">To</Label>
+              <Input
+                sx={{
+                  display: 'block',
+                  width: '100%'
+                }}
+                onChange={handlePropChange('to')}
+                value={elementData.props.to || ''}
+                aria-labelledby="to"
+              />
+            </React.Fragment>
+          )}
+          <h3
+            sx={{
+              fontWeight: 'normal',
+              mt: 4,
+              mb: 0,
+              pt: 4,
+              borderTop: 'thin solid'
+            }}
+          >
+            Styles
+          </h3>
+          {elementData.props.sx && (
+            <React.Fragment>
+              <Label>Padding</Label>
+              <Input
+                sx={{
+                  display: 'block',
+                  width: '100%'
+                }}
+                onChange={handleChange('p')}
+                value={elementData.props.sx.p}
+              />
+              <button onClick={handleRemove('p')}>Remove</button>
+            </React.Fragment>
+          )}
+          <React.Fragment>
+            <Label>Background</Label>
+            <Input
+              sx={{
+                display: 'block',
+                width: '100%'
+              }}
+              onChange={handleChange('background')}
+              value={
+                elementData.props.sx && elementData.props.sx.backgroundColor
+              }
+            />
+            <button onClick={handleRemove('background')}>Remove</button>
+          </React.Fragment>
+          <React.Fragment>
+            <Label>Color</Label>
+            <Input
+              sx={{
+                display: 'block',
+                width: '100%'
+              }}
+              onChange={handleChange('color')}
+              value={elementData.props.sx.color}
+            />
+            <button onClick={handleRemove('color')}>Remove</button>
+          </React.Fragment>
+          <h3
+            sx={{
+              fontWeight: 'normal',
+              mt: 4,
+              mb: 0,
+              pt: 4,
+              borderTop: 'thin solid'
+            }}
+          >
+            Variant
+          </h3>
+          {elementData.props.sx && elementData.props.sx.variant && (
+            <React.Fragment>
+              <Label>
+                {elementData.props.sx.variant.replace('styles.', '')}
+              </Label>
+              <Input
+                sx={{
+                  display: 'block',
+                  width: '100%'
+                }}
+                onChange={handleChange('variant')}
+                value={elementData.props.sx.variant}
+              />
+            </React.Fragment>
+          )}
+          <pre>{JSON.stringify(elementData, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  </div>
+)
--- packages/blocks-ui/src/side-panel.js
+++ packages/blocks-ui/src/side-panel.js
@@ -1,9 +1,8 @@
 /** @jsx jsx */
-import React from 'react'
 import { jsx } from 'theme-ui'
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@reach/tabs'
-import { Label, Input } from '@theme-ui/components'
 
+import EditorPanel from './editor-panel'
 import ThemePanel from './theme-panel'
 import BlocksListing from './blocks-listing'
 
@@ -82,166 +81,18 @@ export default ({
       </TabList>
       <TabPanels>
         <TabPanel>
-          <div
-            sx={{
-              height: '100vh',
-              overflow: 'scroll'
-            }}
-          >
-            {elementData ? (
-              <h3
-                sx={{
-                  textTransform: 'uppercase',
-                  fontSize: 2,
-                  fontWeight: 600,
-                  m: 0,
-                  px: 3,
-                  py: 1,
-                  borderBottom: 'thin solid #e1e6eb'
-                }}
-              >
-                {elementData.name}
-                <button onClick={handleRemoveElement}>Remove</button>
-                <button onClick={handleInsertElement}>Insert</button>
-                <button onClick={handleClone}>Clone</button>
-                {elementData.parentId && (
-                  <button onClick={handleParentSelect}>Parent</button>
-                )}
-              </h3>
-            ) : (
-              <ul>
-                {blocks.map &&
-                  blocks.map(block => (
-                    <li key={block.id} onClick={() => setElementId(block.id)}>
-                      {block.name}
-                    </li>
-                  ))}
-              </ul>
-            )}
-            <div>
-              {elementData && (
-                <div sx={{ px: 3 }}>
-                  {elementData.hasOwnProperty('text') && (
-                    <React.Fragment>
-                      <Label id="text">Text</Label>
-                      <Input
-                        sx={{
-                          display: 'block',
-                          width: '100%'
-                        }}
-                        onChange={handleTextUpdate}
-                        value={elementData.text}
-                        aria-labelledby="text"
-                      />
-                    </React.Fragment>
-                  )}
-                  <h3
-                    sx={{
-                      fontWeight: 'normal',
-                      m: 0,
-                      pb: 2,
-                      pt: 4
-                    }}
-                  >
-                    Props
-                  </h3>
-                  {elementData.props.hasOwnProperty('to') && (
-                    <React.Fragment>
-                      <Label id="to">To</Label>
-                      <Input
-                        sx={{
-                          display: 'block',
-                          width: '100%'
-                        }}
-                        onChange={handlePropChange('to')}
-                        value={elementData.props.to || ''}
-                        aria-labelledby="to"
-                      />
-                    </React.Fragment>
-                  )}
-                  <h3
-                    sx={{
-                      fontWeight: 'normal',
-                      mt: 4,
-                      mb: 0,
-                      pt: 4,
-                      borderTop: 'thin solid'
-                    }}
-                  >
-                    Styles
-                  </h3>
-                  {elementData.props.sx && (
-                    <React.Fragment>
-                      <Label>Padding</Label>
-                      <Input
-                        sx={{
-                          display: 'block',
-                          width: '100%'
-                        }}
-                        onChange={handleChange('p')}
-                        value={elementData.props.sx.p}
-                      />
-                      <button onClick={handleRemove('p')}>Remove</button>
-                    </React.Fragment>
-                  )}
-                  <React.Fragment>
-                    <Label>Background</Label>
-                    <Input
-                      sx={{
-                        display: 'block',
-                        width: '100%'
-                      }}
-                      onChange={handleChange('background')}
-                      value={
-                        elementData.props.sx &&
-                        elementData.props.sx.backgroundColor
-                      }
-                    />
-                    <button onClick={handleRemove('background')}>Remove</button>
-                  </React.Fragment>
-                  <React.Fragment>
-                    <Label>Color</Label>
-                    <Input
-                      sx={{
-                        display: 'block',
-                        width: '100%'
-                      }}
-                      onChange={handleChange('color')}
-                      value={elementData.props.sx.color}
-                    />
-                    <button onClick={handleRemove('color')}>Remove</button>
-                  </React.Fragment>
-                  <h3
-                    sx={{
-                      fontWeight: 'normal',
-                      mt: 4,
-                      mb: 0,
-                      pt: 4,
-                      borderTop: 'thin solid'
-                    }}
-                  >
-                    Variant
-                  </h3>
-                  {elementData.props.sx && elementData.props.sx.variant && (
-                    <React.Fragment>
-                      <Label>
-                        {elementData.props.sx.variant.replace('styles.', '')}
-                      </Label>
-                      <Input
-                        sx={{
-                          display: 'block',
-                          width: '100%'
-                        }}
-                        onChange={handleChange('variant')}
-                        value={elementData.props.sx.variant}
-                      />
-                    </React.Fragment>
-                  )}
-                  <pre>{JSON.stringify(elementData, null, 2)}</pre>
-                </div>
-              )}
-            </div>
-          </div>
+          <EditorPanel
+            elementData={elementData}
+            handleChange={handleChange}
+            handlePropChange={handlePropChange}
+            handleRemove={handleRemove}
+            handleRemoveElement={handleRemoveElement}
+            handleParentSelect={handleParentSelect}
+            handleInsertElement={handleInsertElement}
+            handleClone={handleClone}
+            handleTextUpdate={handleTextUpdate}
+            setElementId={setElementId}
+          />
         </TabPanel>
         <TabPanel>
           {activeTab === 1 ? (
