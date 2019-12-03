2c62a1ab2db0714d1175a1ef7b7525fd26db33e4
John Otander <johnotander@gmail.com>
2019-12-03 15:22:03 -0800

    Use path skip rather than stop to let other plugins run

--- packages/blocks-ui/src/babel-plugins/add-block-imports.js
+++ packages/blocks-ui/src/babel-plugins/add-block-imports.js
@@ -21,7 +21,7 @@ export default (_api, { blocks = [] }) => {
           )
 
           path.replaceWith(importAst)
-          path.stop()
+          path.skip()
         } else {
           path.remove()
         }
