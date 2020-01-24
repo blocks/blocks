f940505526a2eab521a8055cbdb19729bc029f40
John Otander <johnotander@gmail.com>
2020-01-24 09:58:46 -0700

    Add new uuids to cloned child elements
    Closes #212

--- packages/blocks-ui/src/babel-plugins/add-tuid-prop.test.js
+++ packages/blocks-ui/src/babel-plugins/add-tuid-prop.test.js
@@ -12,3 +12,12 @@ test('ignores uuid if it already exists', () => {
 
   expect(result).toEqual('<h1 ___uuid="abc">Hello, world!</h1>')
 })
+
+test('changes uuid when forceUuid is set when it already exists', () => {
+  const result = testPlugin(plugin, '<h1 ___uuid="abc">Hello, world!</h1>', {
+    forceUuid: true
+  })
+
+  expect(result).not.toEqual('<h1 ___uuid="abc">Hello, world!</h1>')
+  expect(result).toMatch(/___uuid/)
+})
--- packages/blocks-ui/src/babel-plugins/test-util.js
+++ packages/blocks-ui/src/babel-plugins/test-util.js
@@ -1,7 +1,8 @@
 import { transform } from '@babel/standalone'
 import babelPluginSyntaxJsx from '@babel/plugin-syntax-jsx'
 
-export const testPlugin = (plugin, src) =>
-  transform(src, {
-    plugins: [babelPluginSyntaxJsx, plugin]
+export const testPlugin = (plugin, src, opts = {}) => {
+  return transform(src, {
+    plugins: [babelPluginSyntaxJsx, [plugin, opts]]
   }).code.replace(/;$/, '')
+}
