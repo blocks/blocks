0bb2c91fdf38cde3555540b28353b99df2528d85
Jordan Overbye <jordanoverbye@gmail.com>
2020-01-25 18:03:10 +1000

    Add title to demo page

--- src/pages/demo.js
+++ src/pages/demo.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Editor from 'blocks-ui'
 import * as Blocks from '@blocks/react'
+
+import SEO from '../components/seo'
 
 const JSX = `
 import React from 'react'
@@ -24,8 +26,11 @@ const Layout = props => {
   return <div className="layout">{props.children}</div>
 }
 
-const Demo = () => {
-  return <Editor src={JSX} blocks={Blocks} layout={Layout} />
-}
+const Demo = () => (
+  <Fragment>
+    <SEO title="Demo" />
+    <Editor src={JSX} blocks={Blocks} layout={Layout} />
+  </Fragment>
+)
 
 export default Demo
