db0398e987d08026d221bf139f65289b8746c2c8
John Otander <johnotander@gmail.com>
2020-01-31 17:52:26 -0800

    Improve prop apply plugin

---
+++ .changeset/weak-lamps-warn.md
@@ -0,0 +1,5 @@
+---
+'blocks-ui': patch
+---
+
+Improve prop apply plugin
---
+++ packages/blocks-ui/src/babel-plugins/apply-prop.test.js
@@ -0,0 +1,22 @@
+import { testPlugin } from './test-util'
+import plugin from './apply-prop'
+
+test('applies a string prop to the element', () => {
+  const result = testPlugin(plugin, '<h1 ___uuid="abc">Hello, world!</h1>', {
+    elementId: 'abc',
+    key: 'foo',
+    value: 'bar'
+  })
+
+  expect(result).toEqual(`<h1 ___uuid="abc" foo="bar">Hello, world!</h1>`)
+})
+
+test('applies a number prop to the element', () => {
+  const result = testPlugin(plugin, '<h1 ___uuid="abc">Hello, world!</h1>', {
+    elementId: 'abc',
+    key: 'foo',
+    value: 123
+  })
+
+  expect(result).toEqual(`<h1 ___uuid="abc" foo={123}>Hello, world!</h1>`)
+})
