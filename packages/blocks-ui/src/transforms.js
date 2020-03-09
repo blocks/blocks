ef8070fc325f6083416048850fb0e220fc69634a
andrew-t-james <ajames20@gmail.com>
2020-03-09 17:29:03 -0500

    Remove log

--- packages/blocks-ui/src/transforms.js
+++ packages/blocks-ui/src/transforms.js
@@ -132,7 +132,6 @@ export const reorderJSXBlocks = (code, drag) => {
 }
 
 export const insertJSXBlock = (code, { blocks, ...drag }) => {
-  console.log('blocks:', blocks)
   const block = blocks[drag.draggableId]
 
   if (!block) {
